c88ee86176c35cf481b23875985aca857dbef837
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-03-13 22:22:27 +0500

    Basic Server Setted Up

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/usersControllers.js";
+import verifyRoles from "../middlewares/verifyRoles.js";
+import { userRoles } from "../config/userRoles.js";
 
 // Routes
 usersRouter.get("/:id", getUserById); //accessable by admin & user only
@@ -14,6 +16,6 @@ usersRouter.get("/", getUsers); //accessable by admin only
 
 usersRouter.put("/:id", updateUser); //accessable by admin only
 
-usersRouter.delete("/:id", deleteUser); //accessable by admin only
+usersRouter.delete("/:id", verifyRoles(userRoles.Admin), deleteUser); //accessable by admin only
 
 export default usersRouter;
